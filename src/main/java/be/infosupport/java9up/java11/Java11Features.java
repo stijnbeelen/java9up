9898ed2e98f2ceb6217d5608c855f7d0178ee56f
StijnB <stijn.beelen@hotmail.com>
2019-12-01 23:05:25 +0100

    Add Java 11 features in code

--- src/main/java/be/infosupport/java9up/java11/Java11Features.java
+++ src/main/java/be/infosupport/java9up/java11/Java11Features.java
@@ -1,4 +1,57 @@
 package be.infosupport.java9up.java11;
 
-public class Java11Features {
+import be.infosupport.java9up.java9.JavaFeatures;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.jetbrains.annotations.NotNull;
+import java.util.Arrays;
+
+
+/**
+ * Class which has a method for the new features of Java 9
+ * Implements {@link JavaFeatures}
+ */
+public class Java11Features implements JavaFeatures {
+    private static final Logger log = LoggerFactory.getLogger(Java11Features.class);
+
+
+    void lambdaVariables(){
+        var arrInteger = new Integer[]{5, 9, 3, 6, 2, 4, 8, 7, 1, null};
+
+        //Java 10
+        var amountOfItemsLargerThan5 = Arrays.stream(arrInteger)
+                .filter(x -> (x != null && x > 5)).count();
+        log.info("There are {} items larger than 5 in our list", amountOfItemsLargerThan5);
+
+        //Java 11
+        amountOfItemsLargerThan5 = Arrays.stream(arrInteger)
+                .filter((@NotNull var a) -> (a > 5)).count();
+        // .filter(@NotNull var x -> x > 5).count(); WILL NOT WORK
+
+        log.info("There are {} items larger than 5 in our list", amountOfItemsLargerThan5);
+    }
+
+    void stringAPIAdditions(){
+        var repeat = "La ".repeat(2) + "Land";
+
+        log.info("Yesterday I was watching the movie {}", repeat);
+
+        var strip = "\n\t  hello   \u2005".strip(); //trim difference: trim will not recognize \u2005 as it is Unicode
+        log.info("{} there!", strip); //also .stripTailing() & .stripLeading()
+
+        var emptyString = "";
+
+        if (emptyString.isBlank()){
+            log.info("The given string was blank!");
+        }
+
+        var multipleLines = "Hello\n dear students\n \n!";
+
+        var amountOfLines = multipleLines.lines().count();
+        log.info("The text contained {} lines", amountOfLines);
+    }
+
+    //Java 11 http client Updates
+
 }
--- src/main/java/be/infosupport/java9up/java13/Java13Features.java
+++ src/main/java/be/infosupport/java9up/java13/Java13Features.java
@@ -3,43 +3,43 @@ package be.infosupport.java9up.java13;
 import java.util.List;
 
 public class Java13Features {
-    void printFormattedString(){
-        System.out.println("""
-                           Hello my dear people
-                           This is how it works
-                           "
-                           \\
-                           """);
-
-        var chars = List.of("a", "b", "c", "d");
-        chars.forEach(c -> System.out.println(getIntValue(c)));
-
-
-        System.out.println("""
-                           {
-                               greeting: "hello",
-                               audience: "text blocks",
-                               punctuation: "!"
-                           }""");
-
-        System.out.println("""
-                           {
-                               greeting: "hello",
-                               audience: "text blocks",
-                               punctuation: "!"
-                           }""");
-
-        System.out.println("""
-        Hallo!""");
-    }
-
-    private static int getIntValue(String mode) {
-        return switch (mode) {
-            case "a", "b":
-                yield 1;
-            case "c", "d":
-            default:
-                yield 2;
-        };
-    }
+//    void printFormattedString(){
+//        System.out.println("""
+//                           Hello my dear people
+//                           This is how it works
+//                           "
+//                           \\
+//                           """);
+//
+//        var chars = List.of("a", "b", "c", "d");
+//        chars.forEach(c -> System.out.println(getIntValue(c)));
+//
+//
+//        System.out.println("""
+//                           {
+//                               greeting: "hello",
+//                               audience: "text blocks",
+//                               punctuation: "!"
+//                           }""");
+//
+//        System.out.println("""
+//                           {
+//                               greeting: "hello",
+//                               audience: "text blocks",
+//                               punctuation: "!"
+//                           }""");
+//
+//        System.out.println("""
+//        Hallo!""");
+//    }
+//
+//    private static int getIntValue(String mode) {
+//        return switch (mode) {
+//            case "a", "b":
+//                yield 1;
+//            case "c", "d":
+//            default:
+//                yield 2;
+//        };
+//    }
 }
